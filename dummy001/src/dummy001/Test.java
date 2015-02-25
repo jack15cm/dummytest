e76dd0cf7fec1e1f691a47e6ebf3587383163e34
User <jack15cm@gmail.com>
2015-02-25 10:32:05 +0800

    yes

--- dummy001/src/dummy001/Test.java
+++ dummy001/src/dummy001/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("modified online!");
-		System.out.println("modified local!");
 
 	}
 
